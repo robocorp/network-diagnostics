52746fffa45183a082214e0472e3aacacaa0d44a
Mika Hänninen <mika.hanninen@gmail.com>
2022-10-06 16:01:07 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -39,10 +39,29 @@ def main():
     # print(dir(SOCK))
     # print(dir(SOCK.context))
     # DIR: SOCK
-    # ['__class__', '__del__', '__delattr__', '__dict__', '__dir__', '__doc__', '__enter__', '__eq__', '__exit__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', '__weakref__', '_accept', '_checkClosed', '_check_connected', '_check_sendfile_params', '_closed', '_connected', '_context', '_create', '_decref_socketios', '_io_refs', '_real_close', '_real_connect', '_sendfile_use_send', '_sendfile_use_sendfile', '_session', '_sslobj', 'accept', 'bind', 'cipher', 'close', 'compression', 'connect', 'connect_ex', 'context', 'detach', 'do_handshake', 'do_handshake_on_connect', 'dup', 'family', 'fileno', 'get_channel_binding', 'get_inheritable', 'getblocking', 'getpeercert', 'getpeername', 'getsockname', 'getsockopt', 'gettimeout', 'ioctl', 'listen', 'makefile', 'pending', 'proto', 'read', 'recv', 'recv_into', 'recvfrom', 'recvfrom_into', 'recvmsg', 'recvmsg_into', 'selected_alpn_protocol', 'selected_npn_protocol', 'send', 'sendall', 'sendfile', 'sendmsg', 'sendto', 'server_hostname', 'server_side', 'session', 'session_reused', 'set_inheritable', 'setblocking', 'setsockopt', 'settimeout', 'share', 'shared_ciphers', 'shutdown', 'suppress_ragged_eofs', 'timeout', 'type', 'unwrap', 'verify_client_post_handshake', 'version', 'write']
+    # ['__class__', '__del__', '__delattr__', '__dict__', '__dir__', '__doc__', '__enter__', '__eq__', '__exit__', '__format__', '__ge__',
+    # '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__',
+    # '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__slots__', '__str__', '__subclasshook__',
+    #  '__weakref__', '_accept', '_checkClosed', '_check_connected', '_check_sendfile_params', '_closed', '_connected', '_context',
+    # '_create', '_decref_socketios', '_io_refs', '_real_close', '_real_connect', '_sendfile_use_send', '_sendfile_use_sendfile',
+    # '_session', '_sslobj', 'accept', 'bind', 'cipher', 'close', 'compression', 'connect', 'connect_ex', 'context', 'detach',
+    # 'do_handshake', 'do_handshake_on_connect', 'dup', 'family', 'fileno', 'get_channel_binding', 'get_inheritable', 'getblocking',
+    #  'getpeercert', 'getpeername', 'getsockname', 'getsockopt', 'gettimeout', 'ioctl', 'listen', 'makefile', 'pending', 'proto', 'read',
+    #  'recv', 'recv_into', 'recvfrom', 'recvfrom_into', 'recvmsg', 'recvmsg_into', 'selected_alpn_protocol', 'selected_npn_protocol',
+    # 'send', 'sendall', 'sendfile', 'sendmsg', 'sendto', 'server_hostname', 'server_side', 'session', 'session_reused', 'set_inheritable',
+    # 'setblocking', 'setsockopt', 'settimeout', 'share', 'shared_ciphers', 'shutdown', 'suppress_ragged_eofs', 'timeout', 'type', 'unwrap',
+    # 'verify_client_post_handshake', 'version', 'write']
     # server_hostname
     # DIR: SOCK.context
-    # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_encode_hostname', '_host_flags', '_load_windows_store_certs', '_msg_callback', '_set_alpn_protocols', '_set_npn_protocols', '_windows_cert_stores', '_wrap_bio', '_wrap_socket', 'cert_store_stats', 'check_hostname', 'get_ca_certs', 'get_ciphers', 'hostname_checks_common_name', 'keylog_filename', 'load_cert_chain', 'load_default_certs', 'load_dh_params', 'load_verify_locations', 'maximum_version', 'minimum_version', 'num_tickets', 'options', 'post_handshake_auth', 'protocol', 'session_stats', 'set_alpn_protocols', 'set_ciphers', 'set_default_verify_paths', 'set_ecdh_curve', 'set_npn_protocols', 'set_servername_callback', 'sni_callback', 'sslobject_class', 'sslsocket_class', 'verify_flags', 'verify_mode', 'wrap_bio', 'wrap_socket']
+    # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__',
+    # '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
+    # '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_encode_hostname',
+    # '_host_flags', '_load_windows_store_certs', '_msg_callback', '_set_alpn_protocols', '_set_npn_protocols', '_windows_cert_stores',
+    # '_wrap_bio', '_wrap_socket', 'cert_store_stats', 'check_hostname', 'get_ca_certs', 'get_ciphers', 'hostname_checks_common_name',
+    # 'keylog_filename', 'load_cert_chain', 'load_default_certs', 'load_dh_params', 'load_verify_locations', 'maximum_version',
+    # 'minimum_version', 'num_tickets', 'options', 'post_handshake_auth', 'protocol', 'session_stats', 'set_alpn_protocols', 'set_ciphers',
+    # 'set_default_verify_paths', 'set_ecdh_curve', 'set_npn_protocols', 'set_servername_callback', 'sni_callback', 'sslobject_class',
+    # 'sslsocket_class', 'verify_flags', 'verify_mode', 'wrap_bio', 'wrap_socket']
     tlscon = SOCK.context
     ciphers = tlscon.get_ciphers()
     for cip in ciphers:
